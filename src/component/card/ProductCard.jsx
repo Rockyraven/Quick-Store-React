16168201276574b0e5b57dc10d2a8336e4245d0b
Rockyraven <89784704+Rockyraven@users.noreply.github.com>
2022-03-30 18:10:39 +0530

    Update ProductCard.jsx

--- src/component/card/ProductCard.jsx
+++ src/component/card/ProductCard.jsx
@@ -2,27 +2,25 @@ import { useNav } from 'context/NavContext'
 import React, { useState } from 'react'
 import './productcard.css'
 export const ProductCard = ({ title, brand, discount, price, actualPrice, imgSrc, rating }) => {
-    
-    const {setcartCount, setwishCount } = useNav()
-    const [ color , setColor ] = useState("")
-    const wishHandler = () =>{
+
+    const { setcartCount, setwishCount } = useNav()
+    const [color, setColor] = useState("")
+    const wishHandler = () => {
         setColor("red");
-        setwishCount(count=>count+1)
+        setwishCount(count => count + 1)
     }
     const cartHandler = () => {
-        setcartCount(count1=>count1+1)
+        setcartCount(count1 => count1 + 1)
 
     }
-    
     return (
         <>
-   
             <div className="product-container">
                 <div className="product-card">
                     <img src={imgSrc} alt="" />
                     <div className="wishlist-box" onClick={wishHandler}   >
-                    <i className="wish-list-icon fa fa-heart" style={{ color: color }}></i>
-                    </div>                   
+                        <i className="wish-list-icon fa fa-heart" style={{ color: color }}></i>
+                    </div>
                     <p className="image-title">{title} </p>
                     <p className="image-subtitle">{brand}</p>
                     <div className="rating-box">
@@ -33,11 +31,11 @@ export const ProductCard = ({ title, brand, discount, price, actualPrice, imgSrc
                         <p className="actual-price"> {actualPrice}</p>
                         <p className="discount">{discount}</p>
                     </div>
-                    <button className='buy' onClick={cartHandler}>ADD TO CARD</button>
+                    <button className='buy' onClick={cartHandler}>Add To Card</button>
 
                 </div>
-                </div>
-            
+            </div>
+
         </>
     )
 }
